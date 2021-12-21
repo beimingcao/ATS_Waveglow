d947aecf324a87caa4d75aef6835e0386b732d40
Beiming <beiming.cao@utexas.edu>
2021-12-21 01:18:55 -0600

    ready for training

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
 
-new_experiment=False
+new_experiment=True
 
 conf_dir=conf/ATS_conf.yaml
 experiments_dir=experiments
 current_exp=current_exp
 
 if [ "$new_experiment" = "True" ];then
-echo "New experiments"
-python3 0_setup.py --conf_dir $conf_dir --exp_dir $experiments_dir --buff_dir $current_exp
-#python3 1_data_loadin.py --conf_dir $conf_dir --buff_dir $current_exp
+echo "New experiments, loading data into current_exp folder"
+#python3 0_setup.py --conf_dir $conf_dir --exp_dir $experiments_dir --buff_dir $current_exp
+#python3 1_data_prepare.py --conf_dir $conf_dir --buff_dir $current_exp
+
+python3 2_data_loadin.py --conf_dir $conf_dir --buff_dir $current_exp
 fi
 
