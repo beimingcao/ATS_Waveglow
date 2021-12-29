087f1df4fd2df2c406c17e21936a334eaa8959db
Beiming <beiming.cao@utexas.edu>
2021-12-29 16:32:17 -0600

    clean-up

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
 new_experiment=False
-training=False
+training=True
 testing=True
+
+save_results=True
+comment="No_comment"
 
 conf_dir=conf/ATS_conf.yaml
 experiments_dir=experiments
@@ -10,9 +13,9 @@ current_exp=current_exp
 
 if [ "$new_experiment" = "True" ];then
 echo "New experiments, loading data into current_exp folder"
-#rm -rf $current_exp
-#python3 0_setup.py --conf_dir $conf_dir --exp_dir $experiments_dir --buff_dir $current_exp
-#python3 1_data_prepare.py --conf_dir $conf_dir --buff_dir $current_exp
+rm -rf $current_exp
+python3 0_setup.py --conf_dir $conf_dir --exp_dir $experiments_dir --buff_dir $current_exp
+python3 1_data_prepare.py --conf_dir $conf_dir --buff_dir $current_exp
 python3 2_data_loadin.py --conf_dir $conf_dir --buff_dir $current_exp
 fi
 if [ "$training" = "True" ];then
@@ -27,4 +30,8 @@ rm -rf $current_exp/RESULTS
 python3 4_test.py --conf_dir $conf_dir --buff_dir $current_exp
 fi
 
-#./5_synthesis.sh
+./5_synthesis.sh
+
+if [ "$save_results" = "True" ];then
+python3 6_cleanup.py --conf_dir $conf_dir --exp_dir $experiments_dir --buff_dir $current_exp --comment $comment
+fi
